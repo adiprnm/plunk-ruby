6e4704219d487149c1b69b36d05c706e75b6bddd
Adi Purnama <adiprnm2014@gmail.com>
2024-09-16 19:54:53 +0700

    Update railtie.rb

--- lib/plunk/action_mailer/railtie.rb
+++ lib/plunk/action_mailer/railtie.rb
@@ -5,7 +5,7 @@ module Plunk
     class Railtie < Rails::Railtie
       initializer 'plunk_action_mailer.add_delivery_method', before: 'action_mailer.set_configs' do
         ActiveSupport.on_load(:action_mailer) do
-          ::ActionMailer::Base.add_delivery_method(:mailtrap, Plunk::ActionMailer::DeliveryMethod)
+          ::ActionMailer::Base.add_delivery_method(:plunk, Plunk::ActionMailer::DeliveryMethod)
         end
       end
     end
